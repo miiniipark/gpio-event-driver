98dd64c2688e5cd9ef4a40ceb617f2609ef696e0
miiniipark <109065263+miiniipark@users.noreply.github.com>
2026-06-08 21:05:06 +0900

    feat: GPIO 버튼 디바운싱 기능 추가
    * Delayed Work 구조체 및 디바운스 시간 정의 추가
    * GPIO IRQ 발생 시 Delayed Work 예약 처리
    * 디바운스 후 버튼 상태 확인 및 LED 토글 수행
    * 모듈 종료 시 delayed work 취소 및 동기화 처리

--- gpio_event_driver.c
+++ gpio_event_driver.c
@@ -6,6 +6,7 @@
 #include <linux/err.h>
 #include <linux/gpio.h>
 #include <linux/interrupt.h>
+#include <linux/workqueue.h>
 
 #define DEV_NAME "gpio_event"
 #define GPIO_BASE 512
@@ -13,6 +14,7 @@
 #define BUTTON_GPIO_BCM 27
 #define LED_GPIO (GPIO_BASE + LED_GPIO_BCM)
 #define BUTTON_GPIO (GPIO_BASE + BUTTON_GPIO_BCM)
+#define DEBOUNCE_MS 20
 
 static dev_t gpio_event_devt;
 static struct cdev gpio_event_cdev;
@@ -21,18 +23,21 @@ static struct device *gpio_event_device;
 static char data[32];
 static int button_irq;
 static int led_state;
+static struct delayed_work button_debounce_work;
 
 static irqreturn_t button_irq_handler(int irq, void *dev_id)
 {
-    int value = gpio_get_value(BUTTON_GPIO);
-
-    if (value) {
-        led_state = !led_state;
-        gpio_set_value(LED_GPIO, led_state);
-        pr_info("Button pressed, LED state: %d\n", led_state);
-    }
-
+    mod_delayed_work(system_wq, &button_debounce_work, msecs_to_jiffies(DEBOUNCE_MS));
     return IRQ_HANDLED;
+}
+
+static void button_debounce_work_func(struct work_struct *work)
+{
+    if (!gpio_get_value(BUTTON_GPIO))
+        return;
+
+    led_state = !led_state;
+    gpio_set_value(LED_GPIO, led_state);
 }
 
 static ssize_t gpio_event_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
@@ -112,6 +117,8 @@ static int __init gpio_event_init(void)
     if (ret)
         goto err_gpio_free_button;
 
+    INIT_DELAYED_WORK(&button_debounce_work, button_debounce_work_func);
+
     button_irq = gpio_to_irq(BUTTON_GPIO);
     if (button_irq < 0) {
         ret = button_irq;
@@ -149,6 +156,7 @@ static void __exit gpio_event_exit(void)
 {
     device_destroy(gpio_event_class, gpio_event_devt);
     free_irq(button_irq, NULL);
+    cancel_delayed_work_sync(&button_debounce_work);
     gpio_free(BUTTON_GPIO);
     gpio_free(LED_GPIO);
     class_destroy(gpio_event_class);
